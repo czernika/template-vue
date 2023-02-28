2c6b554887a89c988c6743e062c5c4d3c72697d7
Aliakseyenka Ihar <aliha.devs@gmail.com>
2023-02-28 20:30:43 +0300

    refactor: typescript features

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <div class="flex justify-center flex-col items-center h-screen text-cyan-700 selection:bg-cyan-600 selection:text-white">
+        <div class="flex justify-center flex-col items-center h-screen text-cyan-700 selection:(bg-cyan-600 text-white)">
             <RouterView />
         </div>
     </div>
--- src/stores/example.ts
+++ src/stores/example.ts
@@ -1,7 +1,11 @@
 import { defineStore } from 'pinia'
 
+interface ExampleStore {
+    title: string
+}
+
 export const useExampleStore = defineStore('example', {
     state: () => ({
         title: 'Hello World',
-    }),
+    } as ExampleStore),
 })
